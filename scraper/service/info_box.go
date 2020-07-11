20557aed776a7256e8331d475e623c4b12452893
Sebastián Salata R-T <sa.salatart@gmail.com>
2020-07-11 14:36:32 -0400

    feat(scraper): support mixture between participant-specific and overall numbers

--- scraper/service/info_box.go
+++ scraper/service/info_box.go
@@ -8,7 +8,7 @@ import (
 
 const infoBoxSelector = ".infobox.vevent > tbody"
 
-func setInfoBoxID(c *colly.Collector, title string, id string) {
+func subscribeInfoBoxID(c *colly.Collector, title string, id string) {
 	c.OnHTML(infoBoxSelector, func(e *colly.HTMLElement) {
 		e.ForEachWithBreak("th", func(_ int, c *colly.HTMLElement) bool {
 			if strings.ToLower(c.Text) != strings.ToLower(title) {
--- scraper/service/numbers.go
+++ scraper/service/numbers.go
@@ -11,7 +11,7 @@ import (
 
 func subscribeNumbersFor(c *colly.Collector, trTitle string, storeIn *domain.SideNumbers) {
 	trID := "batcoms-" + strings.ReplaceAll(strings.ToLower(trTitle), " ", "")
-	setInfoBoxID(c, trTitle, trID)
+	subscribeInfoBoxID(c, trTitle, trID)
 	twoSides := false
 	c.OnHTML(fmt.Sprintf("#%s > td:first-child", trID), func(e *colly.HTMLElement) {
 		storeIn.A = parser.Clean(e.Text)
@@ -19,6 +19,10 @@ func subscribeNumbersFor(c *colly.Collector, trTitle string, storeIn *domain.Sid
 	c.OnHTML(fmt.Sprintf("#%s > td:nth-child(2)", trID), func(e *colly.HTMLElement) {
 		storeIn.B = parser.Clean(e.Text)
 		twoSides = true
+	})
+	c.OnHTML(fmt.Sprintf("#%s + tr>td[colspan='2']", trID), func(e *colly.HTMLElement) {
+		textInTags := e.DOM.ChildrenFiltered("*").Text()
+		storeIn.AB = parser.Clean(strings.ReplaceAll(e.Text, textInTags, ""))
 	})
 	c.OnScraped(func(_ *colly.Response) {
 		if twoSides {
