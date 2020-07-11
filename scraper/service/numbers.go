24a6ba885884f8fab7f007b2d97a9faaa4059d50
Sebastián Salata R-T <sa.salatart@gmail.com>
2020-07-11 10:00:50 -0400

    feat(scraper): support non-participant specific numbers

--- scraper/service/numbers.go
+++ scraper/service/numbers.go
@@ -12,11 +12,21 @@ import (
 func subscribeNumbersFor(c *colly.Collector, trTitle string, storeIn *domain.SideNumbers) {
 	trID := "batcoms-" + strings.ReplaceAll(strings.ToLower(trTitle), " ", "")
 	setInfoBoxID(c, trTitle, trID)
+	twoSides := false
 	c.OnHTML(fmt.Sprintf("#%s > td:first-child", trID), func(e *colly.HTMLElement) {
 		storeIn.A = parser.Clean(e.Text)
 	})
 	c.OnHTML(fmt.Sprintf("#%s > td:nth-child(2)", trID), func(e *colly.HTMLElement) {
 		storeIn.B = parser.Clean(e.Text)
+		twoSides = true
+	})
+	c.OnScraped(func(_ *colly.Response) {
+		if twoSides {
+			return
+		}
+		storeIn.AB = storeIn.A
+		storeIn.A = ""
+		storeIn.B = ""
 	})
 }
 
