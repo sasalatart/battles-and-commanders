8868686bf9ba592a6cab77dd428b86cf4c83010f
Sebastián Salata R-T <sa.salatart@gmail.com>
2020-08-31 20:30:00 -0400

    refactor(postgresql): encapsulate reset db logic

--- cmd/seeder/main.go
+++ cmd/seeder/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/sasalatart/batcoms/config"
 	"github.com/sasalatart/batcoms/services/seeder"
 	"github.com/sasalatart/batcoms/store/postgresql"
-	"github.com/sasalatart/batcoms/store/postgresql/schema"
 	"github.com/spf13/viper"
 )
 
@@ -18,20 +17,7 @@ func main() {
 	db := postgresql.Connect()
 	defer db.Close()
 
-	db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;`)
-	schemas := []interface{}{&schema.BattleCommanderFaction{}, &schema.BattleFaction{}, &schema.BattleCommander{}, &schema.Faction{}, &schema.Commander{}, &schema.Battle{}}
-	for _, s := range schemas {
-		db.DropTableIfExists(s)
-		db.AutoMigrate(s)
-	}
-	db.Model(&schema.BattleFaction{}).AddForeignKey("battle_id", "battles(id)", "CASCADE", "CASCADE")
-	db.Model(&schema.BattleFaction{}).AddForeignKey("faction_id", "factions(id)", "CASCADE", "CASCADE")
-	db.Model(&schema.BattleCommander{}).AddForeignKey("battle_id", "battles(id)", "CASCADE", "CASCADE")
-	db.Model(&schema.BattleCommander{}).AddForeignKey("commander_id", "commanders(id)", "CASCADE", "CASCADE")
-	db.Model(&schema.BattleCommanderFaction{}).AddForeignKey("battle_id", "battles(id)", "CASCADE", "CASCADE")
-	db.Model(&schema.BattleCommanderFaction{}).AddForeignKey("commander_id", "commanders(id)", "CASCADE", "CASCADE")
-	db.Model(&schema.BattleCommanderFaction{}).AddForeignKey("faction_id", "factions(id)", "CASCADE", "CASCADE")
-
+	postgresql.Reset(db)
 	battlesFileName := viper.GetString("SCRAPER_RESULTS.BATTLES")
 	participantsFileName := viper.GetString("SCRAPER_RESULTS.PARTICIPANTS")
 	importedData, err := seeder.JSONImport(battlesFileName, participantsFileName)
